7eaa0b3f2a2557aee1408298cc3f0a973c760adc
Nick Sweeting <git@nicksweeting.com>
2016-04-07 14:29:17 -0500

    logging improvements to socks commands

--- socks.fish
+++ socks.fish
@@ -1,7 +1,17 @@
 function socks
-    echo "[+] Starting socks proxy on port 1080."
+    quiet tcping -t 2 nicksweeting.com 80
+    and echo "[√] Able to reach nicksweeting.com:80"
+    or begin
+        echo "[X] Unable to reach nicksweeting.com:80"
+        tcping -t 2 nicksweeting.com 80
+        return
+    end
+
+    echo "[*] Changing network settings for Wi-Fi and Ethernet"
     sudo networksetup -setsocksfirewallproxystate Wi-Fi on
     quiet sudo networksetup -setsocksfirewallproxystate Ethernet on
+
+    echo "[+] Starting socks proxy on port 1080."
     switch (count $argv)
     case '1'
         ssh -N -n -g -D 1080 nick@$argv
--- socksoff.fish
+++ socksoff.fish
@@ -1,4 +1,5 @@
 function socksoff
+    echo "[*] Changing Wi-Fi and Ethernet network settings back"
 	sudo networksetup -setsocksfirewallproxystate Wi-Fi off
     quiet sudo networksetup -setsocksfirewallproxystate Ethernet off
     k "ssh -N -n -g -D 1080"
