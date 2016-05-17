01bb9a7175b77ee7a8c9dab9607efa1c4f0734d7
Nick Sweeting <git@nicksweeting.com>
2016-05-17 11:49:54 -0700

    update stop.all to stop new services

--- all.stop.fish
+++ all.stop.fish
@@ -1,8 +1,13 @@
 function all.stop
 	dns.stop
-mysql.stop
-nginx.stop
-php.stop
-postgresql.stop
-redis.stop
+    mysql.stop
+    postgresql.stop
+    nginx.stop
+    php.stop
+    celery.stop
+    squid.stop
+    tornado.stop
+    redis.stop
+    mongo.stop
+    # bonjour.stop
 end
