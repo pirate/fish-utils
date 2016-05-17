2468b4190a7648502914c8b4cc6d0123a36e4edd
Nick Sweeting <git@nicksweeting.com>
2016-05-17 11:50:08 -0700

    fix bugs in open, find_file, and find_app

--- find_file.fish
+++ find_file.fish
@@ -1,18 +1,30 @@
 function find_file --description "Find [file] in [directory]"
+    # collect arguments
+    set -l query $argv[1]
     set -l folder "."
     test (count $argv) -gt 1; and set folder "$argv[2]"
-    set -l query $argv[1]
+
+    # search current folder for file
     set -l CLICOLOR 0
     set results (/bin/ls "$folder" | grep -i "$query")
-    if not test $results
-        set results (/bin/ls )
+    if not test "$results"
+        return 1
+        # uncomment to change file not found behavior to list dir instead of exiting 1 immediately
+        # set results (/bin/ls)
+    end
+
+    # if multiple files found, query user for choice (using stderr)
     while test (count $results) -gt 1
         for line in $results
-            echo "$line" | cgrep $query 1>&2
+            echo "$line" | cgrep $query 1>&2; or echo "$line" 1>&2
         end
         read -p 'echo $yellow"Pick file: "$normal' query
-        set results (for app in $results; echo $app | ggrep -i "$query"; end)
+        set results (for app in $results;
+                        echo "$app" | ggrep -i "$query";
+                     end)
     end
+
+    # send result to stdout or exit 1
     not test $results; and return 1
     echo "$results"
 end
--- o.fish
+++ o.fish
@@ -4,8 +4,8 @@ function o --description "Open [app] [file]"
         open -a (find_app $argv[1]) $argv[2]
     case 1
         quiet open "$argv"
-        or quiet open (find_file "$argv")
-        or quiet open (find_app "$argv")
+        or quiet open \"(find_file "$argv")\"
+        or quiet open \"(find_app "$argv")\"
         or return 1
     case 0
         quiet open .
