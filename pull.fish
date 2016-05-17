90bcd846af841ed176f0019e30ffcb125ded7298
Nick Sweeting <git@nicksweeting.com>
2016-05-17 11:50:18 -0700

    hardcode pull command to pull from old server

--- pull.fish
+++ pull.fish
@@ -1,7 +1,7 @@
 function pull --description 'Pull specified file from nicksweeting.com to specified path on local (or ~/Desktop/pull by default)'
 	if test (count $argv) -gt 1
-        scp -r -P 44 root@nicksweeting.com:$argv[1] $argv[2]; and subl $argv[2]
+        scp -r -P 44 root@162.243.151.39:$argv[1] $argv[2]; and subl $argv[2]
     else
-        scp -r -P 44 root@nicksweeting.com:$argv[1] /Users/nick/Desktop/pull; and subl /Users/nick/Desktop/pull
+        scp -r -P 44 root@162.243.151.39:$argv[1] /Users/nick/Desktop/pull; and subl /Users/nick/Desktop/pull
     end
 end
