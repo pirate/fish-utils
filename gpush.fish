c054a932b1f4f8d9b3d6c1d4521c78d59611f927
Nick Sweeting <git@nicksweeting.com>
2016-09-19 23:18:04 -0400

    minor fixes to git colorization

--- gpush.fish
+++ gpush.fish
@@ -1,7 +1,7 @@
 function gpush
 	git push --set-upstream origin (_git_branch_name) $argv 2>&1 | \
     regex 's/Everything up-to-date\n/'$green'Everything up-to-date'$normal'/gm' | \
-    regex 's/Branch master set up to track remote branch master from origin by rebasing.//g' | \
+    regex 's/Branch master set up to track remote branch .*//g' | \
     regex 's/\.\./'$green'..'$normal'/g' | \
     regex 's/-\>/'$green'->'$normal'/g'
 end
--- gs.fish
+++ gs.fish
@@ -4,7 +4,7 @@ function gs
     regex 's/  \(use "git .*$//g' | \
     regex 's/no changes added to commit .*$//g' | \
     regex 's/^\s+$\s*$//gm' | \
-    regex 's/nothing to commit/'$green'nothing to commit'$normal'/g' | \
+    regex 's/nothing to commit/'$green'Nothing to commit'$normal'/g' | \
     regex 's/Changes to be committed:/\n----------------------------------\n'$green'Staged'$normal':/g' | \
     regex 's/Changes not staged for commit:/\n----------------------------------\n'$red'Not staged'$normal':/g' | \
     regex 's/^Untracked files:/\n----------------------------------\n'$cyan'Untracked'$normal':/g'
