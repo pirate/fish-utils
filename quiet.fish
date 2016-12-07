6e657c3f7f4308b1289d605ff7566b8e6d864ddd
Nick Sweeting <git@nicksweeting.com>
2016-12-06 21:44:29 -0500

    fix argument whitespace quoting in quiet command

--- quiet.fish
+++ quiet.fish
@@ -1,3 +1,13 @@
 function quiet
-	eval "$argv 2>&1 > /dev/null" 2>&1 > /dev/null; and return 0; or return 1
+    if not count $argv > /dev/null
+        return 0
+    end
+
+    # quote every argument so spacing is preserved
+    set evalstr ''
+    for arg in $argv
+        set evalstr "$evalstr \"$arg\""
+    end
+    # eval the command with output silenced
+    eval "$evalstr 2>&1 > /dev/null" 2>&1 > /dev/null; and return 0; or return 1
 end
