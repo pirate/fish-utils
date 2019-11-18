8c59ddff95fec3cfdd20be42016d5d0f39cd10c7
Nick Sweeting <git@sweeting.me>
2019-11-18 12:21:45 -0500

    Update __fish_command_not_found_handler.fish

--- __fish_command_not_found_handler.fish
+++ __fish_command_not_found_handler.fish
@@ -1,5 +1,7 @@
 function __fish_command_not_found_handler --on-event fish_command_not_found
-    # echo "["$red"?"$normal"]"$yellow": "$normal"'$argv'" >&2;
-    # history $argv | head | cgrep $argv >&2;
-    # echo './'(find_file $argv)
+    echo "["$red"?"$normal"]"$yellow": "$normal"'$argv'" >&2;
+    history $argv | head | cgrep $argv >&2;
+    echo './'(find_file $argv)
+
+    # autojump $argv
 end
