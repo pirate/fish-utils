640928cad9d9c5ff5aca2de3a6afb8d9def79b49
Nick Sweeting <git@sweeting.me>
2020-09-30 14:48:08 -0400

    improve command not found handler

--- __fish_command_not_found_handler.fish
+++ __fish_command_not_found_handler.fish
@@ -1,7 +1,9 @@
 function __fish_command_not_found_handler --on-event fish_command_not_found
-    echo "["$red"?"$normal"]"$yellow": "$normal"'$argv'" >&2;
+    # echo "["$red"?"$normal"]"$yellow": "$normal"'$argv'" >&2;
+    # history $argv | head | cgrep $argv >&2;
+    # echo './'(find_file $argv)
+
+    echo "["$red"?"$normal"]"$yellow": Unrecognized command $red'$argv'$yellow did you mean: $normal" >&2
     history $argv | head | cgrep $argv >&2;
     echo './'(find_file $argv)
-
-    # autojump $argv
 end
